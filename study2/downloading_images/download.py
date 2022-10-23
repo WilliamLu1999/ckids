c83c1e9a31401127483197fa1d753e7d20ea6660
ZexunYao <115748180+ZexunYao@users.noreply.github.com>
2022-10-22 23:22:04 -0700

    Create download.py

---
+++ study2/downloading_images/download.py
@@ -0,0 +1,53 @@
+import pandas as pd
+import numpy as np
+import urllib.request 
+import os
+import glob
+
+file_paths=[]
+path_2022_03 = "/project/ll_774_951/uk_ru/twitter/data/2022-03/*.csv"
+for fname in glob.glob(path_2022_03):
+    file_paths.append(fname)
+path_2022_04 = "/project/ll_774_951/uk_ru/twitter/data/2022-04/*.csv"
+for fname in glob.glob(path_2022_04):
+    file_paths.append(fname)
+file_paths.sort()
+file_paths_1 = file_paths[358:458]
+
+for file in file_paths_1:
+    df = pd.read_csv(file,  engine='python', error_bad_lines=False)
+    
+    media_urls = df[['media_urls']]
+    rt_media_urls = df[['rt_media_urls']]
+    q_media_urls = df[['q_media_urls']]
+    
+    download_1 = media_urls.loc[df.media_urls.str.contains('http', na=False)]
+    download_2 = rt_media_urls.loc[df.rt_media_urls.str.contains('http', na=False)]
+    download_3 = q_media_urls.loc[df.q_media_urls.str.contains('http', na=False)]
+    
+    download_list_1 = download_1["media_urls"].tolist()
+    download_list_2 = download_2["rt_media_urls"].tolist()
+    download_list_3 = download_3["q_media_urls"].tolist()
+    
+    special_char = ",]['"
+    downloads_1 = [''.join(x for x in string if not x in special_char) for string in download_list_1]
+    downloads_2 = [''.join(x for x in string if not x in special_char) for string in download_list_2]
+    downloads_3 = [''.join(x for x in string if not x in special_char) for string in download_list_3]
+    
+    for link in downloads_1:
+        try:
+            urllib.request.urlretrieve(link, "/project/ll_774_951/uk_ru/twitter/twitter_images/" + os.path.basename(link))
+        except:
+            pass
+    
+    for link in downloads_2:
+        try:
+            urllib.request.urlretrieve(link, "/project/ll_774_951/uk_ru/twitter/twitter_images/" + os.path.basename(link))
+        except:
+            pass
+    
+    for link in downloads_3:
+        try:
+            urllib.request.urlretrieve(link, "/project/ll_774_951/uk_ru/twitter/twitter_images/" + os.path.basename(link))
+        except:
+            pass
