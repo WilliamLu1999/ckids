4cb09f95d790bbeef258c5f9edb044cc715dfdb4
William Lu <weilianlu@gmail.com>
2022-10-31 23:00:32 -0700

    tweet_id_screenshots

---
+++ study2/identifying_screenshots/tweet_id_download.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import urllib.request 
+import os
+import glob
+import os.path
+import csv
+import re
+import pathlib
+import mimetypes
+from urllib.request import urlopen
+from pathlib import Path
+
+
+df = pd.read_csv('/Users/William/Downloads/tweet_id_url.csv') # 记得改
+# dff = df.values.tolist()
+counter = 4001
+for link in df['0'][4000::]:
+    path = f"/Users/William/Downloads/tweet_id_pic/{counter}"+Path(link).suffix
+    try:
+        urllib.request.urlretrieve(link,path)
+        counter += 1
+    except:
+        pass
